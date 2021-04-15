159dc0d347ad2e486d0d5839fa3cdc72105a042f
adriigc10 <adriangarciacubas@hotmail.com>
2021-04-15 13:04:33 +0200

    Excepcion DatoIncorrecto añadida

---
+++ Practica 4/practica4/src/main/java/practica4/DatoIncorrectoException.java
@@ -0,0 +1,9 @@
+package practica4;
+
+/**
+ * Metodo de excepcion DatoIncorrectoException
+ * @author Borja Cuevas Cuesta y Adrian Garcia Cubas
+ *
+ */
+@SuppressWarnings("serial")
+public class DatoIncorrectoException extends RuntimeException{}
