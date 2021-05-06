cda8555dc72357bf8cb9abb6a07ca64073cb9a92
adriigc10 <adriangarciacubas@hotmail.com>
2021-05-06 12:06:21 +0200

    Eliminacion de carpeta

--- practica5/src/main/java/es/unican/is2/practica5/saldoInsuficienteException.java
+++
@@ -1,9 +0,0 @@
-package es.unican.is2.practica5;
-
-@SuppressWarnings("serial")
-public class saldoInsuficienteException extends RuntimeException {
-
-	public saldoInsuficienteException (String mensaje) {
-		super(mensaje);
-	}
-}
